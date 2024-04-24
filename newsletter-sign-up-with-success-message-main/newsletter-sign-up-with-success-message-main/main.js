4a35bceeef77f780274651564d414905965c3003
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-04-24 19:05:36 +0430

    Add javascript file

---
+++ newsletter-sign-up-with-success-message-main/newsletter-sign-up-with-success-message-main/main.js
@@ -0,0 +1,7 @@
+const boxmodal = document.querySelector(".box-modal"), 
+showbtn = document.getElementsByClassName("showbtn"), 
+closebtn = document.getElementsByClassName("dismiss");
+
+boxmodal.addEventListener('click', function(){
+  console.log("Hi"); 
+})

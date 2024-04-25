630660c1af61f4c5e7bae3d511ab10c190ba8898
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-04-25 17:49:49 +0430

    Add email validation for desktop version

--- newsletter-sign-up-with-success-message-main/newsletter-sign-up-with-success-message-main/main.js
+++ newsletter-sign-up-with-success-message-main/newsletter-sign-up-with-success-message-main/main.js
@@ -1,7 +1,40 @@
-const boxmodal = document.querySelector(".box-modal"), 
-showbtn = document.getElementsByClassName("showbtn"), 
-closebtn = document.getElementsByClassName("dismiss");
 
-boxmodal.addEventListener('click', function(){
-  console.log("Hi"); 
-})
+
+let overlayLayer = document.getElementsByTagName('span')[0]; 
+let box = document.querySelector('.box-modal'); 
+let btn = document.querySelector('.showbtn'); 
+let disbtn = document.querySelector('.dismiss'); 
+let gmail = document.querySelector("#gmail"); 
+let invalid = document.querySelector(".invalid"); 
+
+
+// btn.addEventListener('click', showMessage()); 
+function showMessage(){
+     let emailInput = document.getElementById('input-email');
+    if(isValidEmail(emailInput.value)){
+     
+    console.log("inner")
+      overlayLayer.classList.add('overlay'); 
+      box.style.display = 'block';  
+      gmail.textContent = emailInput;  
+      console.log(emailInput); 
+      invalid.style.display = 'none'; 
+      emailInput.style.border = "0.5px solid rgba(0, 0, 0, 0.305)";
+      emailInput.value = ''; 
+    }else{
+      invalid.style.display = 'inline'; 
+      emailInput.style.border = "1px solid red";
+    }
+}
+
+function dismissMessage(){
+  overlayLayer.classList.remove('overlay'); 
+  box.style.display = 'none'; 
+}
+
+
+function isValidEmail(email) {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return regex.test(email);
+}
+

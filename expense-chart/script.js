62eb17a393b04e0811ff0fd346046e33571962a6
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-07-12 16:00:09 +0430

    Update expense chart challenge
    The expense chart was update to get the data from json file and update the current day based on today

--- expense-chart/script.js
+++ expense-chart/script.js
@@ -1,17 +1,27 @@
-const charts = document.querySelectorAll('.chart'); 
+const charts = document.querySelectorAll('.chart');
 
-const date = new Date(); 
-const today = date.getDay(); 
+// Fetch the data from the JSON file
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Extract the 'amount' values from the JSON data
+    let amounts = data.map(item => item.amount);
+    let days = data.map(item => item.day); 
 
-const daysOfweek = ['sun', 'mon','tue','wed', 'thu', 'fri', 'sat']; 
+    const date = new Date();
+    const today = date.getDay();
 
-const currentDay = daysOfweek[today]; 
+    const currentDay = days[today];
 
+    charts.forEach((chart, i) => {
+      if (chart.classList.contains(currentDay)) {
+        chart.style.background = 'hsl(186, 34%, 60%)';
+      }
 
-charts.forEach(chart =>{
-  if(chart.classList.contains(currentDay)){
-    chart.style.background = 'hsl(186, 34%, 60%)';
-  }
-})
-
-
+      // Use the 'amounts' array from the JSON data
+      chart.style.height = `${amounts[i]}%`;
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });

ab88a01373f22d07e362873d667857db1a17a64d
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-07-14 22:10:00 +0430

    Refactor Faq accordion project

--- faq-accordion/script.js
+++ faq-accordion/script.js
@@ -1,22 +1,10 @@
-const collapseButtons = document.querySelectorAll('.collapse');
-const paragraphs = document.querySelectorAll('p');
+const details = document.querySelectorAll('details');
 
-collapseButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    /**
- * Loop through each paragraph element and toggle the 'hide' class based on the index.
- * If the index matches the current iteration, toggle the 'hide' class and update the button's src attribute.
- * Otherwise, add the 'hide' class to the paragraph and update the corresponding button's src attribute.
- */
-    paragraphs.forEach((p, i) => {
-      if (i === index) {
-        p.classList.toggle('hide');
-        button.src = p.classList.contains('hide')
-          ? './assets/images/icon-plus.svg'
-          : './assets/images/icon-minus.svg';
-      } else {
-        p.classList.add('hide');
-        collapseButtons[i].src = './assets/images/icon-plus.svg';
+details.forEach((detail) => {
+  detail.querySelector('summary').addEventListener('click', () => {
+    details.forEach((d) => {
+      if (d !== detail) {
+        d.removeAttribute('open');
       }
     });
   });

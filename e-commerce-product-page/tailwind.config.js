dad80904317885552be188f1f2fb92f743a62b03
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-11-30 04:11:28 +0430

    feat(e-commerce-product): develop e-commerce product page challenge

--- e-commerce-product-page/tailwind.config.js
+++ e-commerce-product-page/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        Orange: "hsl(26, 100%, 55%)", 
+        paleOrange: "hsl(25, 100%, 94%)", 
+        veryDarkBlue: "hsl(220, 13%, 13%)",
+        darkGrayishBlue: "hsl(219, 9%, 45%)",
+        grayishBlue: "hsl(220, 14%, 75%)",
+        lightGrayishBlue: "hsl(223, 64%, 98%)",
+        white: "hsl(0, 0%, 100%)"
+      }, 
+
+      fontFamily:{
+        "Kumb": ["Kumbh Sans", "sans-serif"
+        ]
+      }
+    },
   },
   plugins: [],
 }
